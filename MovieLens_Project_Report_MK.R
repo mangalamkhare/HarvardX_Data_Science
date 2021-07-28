2804c1876a35bb3fe8f6b6e060e5c5e5d82724a9
mangalamkhare <mangalam.khare@gmail.com>
2021-07-28 17:14:23 +0530

    Add files via upload

---
+++ MovieLens_Project_Report_MK.R
@@ -0,0 +1,401 @@
+# Mangalam Khare
+# MovieLens Project 
+# "23rd July 2021"
+
+#############################################################
+# Create edx set, validation set
+#############################################################
+
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+
+library(tidyverse)
+library(caret)
+library(data.table)
+
+library(ggplot2)
+library(lubridate)
+library(dslabs)
+
+# MovieLens 10M dataset:
+
+dl <- tempfile()
+download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
+
+ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-10M100K/ratings.dat"))),
+                 col.names = c("userId", "movieId", "rating", "timestamp"))
+
+movies <- str_split_fixed(readLines(unzip(dl, "ml-10M100K/movies.dat")), "\\::", 3)
+colnames(movies) <- c("movieId", "title", "genres")
+
+# if using R 3.6 or earlier:
+#movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(levels(movieId))[movieId],
+# title = as.character(title),
+# genres = as.character(genres))
+# if using R 4.0 or later:
+movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(movieId),
+                                           title = as.character(title),
+                                           genres = as.character(genres))
+
+
+ratings_tab <- as.data.frame(ratings) %>% 
+                    mutate(movieId =as.numeric(movieId),
+                    userId = as.numeric(userId), 
+                    rating = as.numeric(rating), 
+                    timestamp = as.numeric(timestamp))
+
+movielens <- left_join(ratings_tab, movies, by = "movieId")
+
+head(movielens)
+
+# Split the dataset iinto training and test tests
+# Validation set will be 10% of MovieLens data
+
+set.seed(1, sample.kind="Rounding") 
+test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
+edx <- movielens[-test_index,]
+temp <- movielens[test_index,]
+
+
+# Make sure userId and movieId in validation set are also in edx set
+
+validation <- temp %>% 
+  semi_join(edx, by = "movieId") %>%
+  semi_join(edx, by = "userId")
+
+# Add rows removed from validation set back into edx set
+
+removed <- anti_join(temp, validation)
+edx <- rbind(edx, removed)
+
+rm(dl, ratings, movies, test_index, temp, movielens, removed)
+
+#Check few rows of data set
+head(edx) 
+
+#Check Dimensions and Summary stats
+
+# Rows Columns
+dim(edx)
+
+# Data set Summary
+summary(edx)
+
+#add a new column year to the edx and validation data set by splitting Title column
+edx <- edx %>% mutate(year = as.numeric(str_sub(title,-5,-2)))
+validation <- validation %>% mutate(year = as.numeric(str_sub(title,-5,-2)))
+
+dim(edx)
+head(edx)
+
+# check for the number of unique movies and users in the edx dataset 
+# The total no of unique users are approx 69878 and 10677 movies
+
+edx %>%
+  summarize(n_users = n_distinct(userId), 
+            n_movies = n_distinct(movieId))
+
+## Define the function for RMSE
+
+RMSE <- function(true_ratings, predicted_ratings){
+  sqrt(mean((true_ratings-predicted_ratings)^2))
+}
+
+
+# Ratings dist
+edx %>%
+  ggplot(aes(rating)) +
+  geom_histogram(binwidth = 0.25, color = "black") +
+  scale_x_discrete(limits = c(seq(0.5,5,0.5))) +
+  scale_y_continuous(breaks = c(seq(0, 3000000, 200000))) +
+  ggtitle("Ratings distribution")
+
+# Movies distribution
+
+edx %>%
+  count(movieId) %>%
+  ggplot(aes(n)) +
+  geom_histogram(bins = 30, color = "black") +
+  scale_x_log10() +
+  xlab("Number of ratings") +
+  ylab("Number of movies") +
+  ggtitle("Number of ratings per movie")
+
+# User's Distribution
+edx %>% count(userId) %>% 
+  ggplot(aes(n)) + 
+  geom_histogram(bins = 25, color = "black") + 
+  scale_x_log10() + 
+  xlab("Number of ratings") + 
+  ylab("Number of users") +
+  ggtitle("Number of ratings given by users")
+
+
+# Modelling and Analysis
+
+## Split the edx dataset into training and test tests
+## test set will be 10% of edx data
+
+set.seed(1, sample.kind="Rounding") 
+test_index <- createDataPartition(y = edx$rating, times = 1, p = 0.1, list = FALSE)
+edx_train <- edx[-test_index,]
+edx_temp <- edx[test_index,]
+
+
+# Make sure userId and movieId in test set are also in train set
+
+edx_test <- edx_temp %>% 
+  semi_join(edx_train, by = "movieId") %>%
+  semi_join(edx_train, by = "userId")
+
+# Add rows removed from validation set back into edx set
+
+removed <- anti_join(edx_temp, edx_test)
+edx_train <- rbind(edx_train, removed)
+
+rm(test_index, edx_temp,removed)
+
+head(edx_train)
+
+head(edx_test)
+
+dim(edx_train)
+dim(edx_test)
+
+#Simple : Average movie rating model
+mu <- mean(edx_train$rating)
+mu
+
+rmse <- RMSE(edx_train$rating, mu)
+rmse
+
+
+#Check results, Save prediction in data frame
+
+rmse_results <- data_frame(Method = " Average movie rating model", 
+                           RMSE = rmse)
+rmse_results %>% knitr::kable()
+
+
+# Movie effect model
+
+# Number of movies with the computed b_i
+
+movie_avgs <- edx_train %>%
+  group_by(movieId) %>%
+  summarize(b_i = mean(rating - mu))
+
+qplot(b_i, 
+      bins = 15, 
+      data = movie_avgs, color = I("black"),
+      ylab = "Number of movies",
+      main = "Number of movies with the computed b_i")
+
+
+# Test and save rmse results 
+
+predicted_ratings <- mu +  edx_test %>%
+  left_join(movie_avgs, by='movieId') %>%
+  pull(b_i)
+
+rmse <- RMSE(predicted_ratings, edx_test$rating)
+
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(Method="Movie effect model",  
+                                     RMSE = rmse))
+# Check results
+rmse_results %>% knitr::kable()
+
+# Model considering Movie effect , user effect 
+
+# Plot user effect 
+
+user_avgs<- edx_train %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  group_by(userId) %>%
+  summarize(b_u = mean(rating - mu - b_i))
+
+qplot(b_u, 
+      bins = 30, 
+      data = user_avgs, color = I("black"),
+      ylab = "Number of users",
+      main = "Number of users with the computed b_u")
+
+
+# Test and save rmse results 
+
+predicted_ratings <- edx_test%>%
+  left_join(movie_avgs, by='movieId') %>%
+  left_join(user_avgs, by='userId') %>%
+  mutate(pred = mu + b_i + b_u) %>%
+  pull(pred)
+
+rmse <- RMSE(predicted_ratings, edx_test$rating)
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(Method="Movie and user effect model",
+                                     RMSE = rmse))
+
+# Check result
+rmse_results %>% knitr::kable()
+
+## Regularized Movie and user effect model
+
+# We will start by taking a few lambda values starting from zero
+lambdas <- seq(0, 10, 0.25)
+
+rmses <- sapply(lambdas, function(l){
+  
+  mu <- mean(edx_train$rating)
+  
+  b_i <- edx_train %>% 
+    group_by(movieId) %>%
+    summarize(b_i = sum(rating - mu)/(n()+l))
+  
+  b_u <- edx_train %>% 
+    left_join(b_i, by="movieId") %>%
+    group_by(userId) %>%
+    summarize(b_u = sum(rating - b_i - mu)/(n()+l))
+  
+  predicted_ratings <- 
+    edx_test %>% 
+    left_join(b_i, by = "movieId") %>%
+    left_join(b_u, by = "userId") %>%
+    mutate(pred = mu + b_i + b_u) %>%
+    pull(pred)
+  
+  return(RMSE(predicted_ratings, edx_test$rating))
+})
+
+
+# Plot rmses vs lambdas to select the optimal lambda                                                             
+qplot(lambdas, rmses)  
+
+
+# The optimal lambda                                                             
+lambda <- lambdas[which.min(rmses)]
+lambda
+
+# Test and save results                                                             
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(Method="Regularized movie and user effect model",  
+                                     RMSE = min(rmses)))
+
+# Check results
+rmse_results %>% knitr::kable()
+
+# Regularized Movie and user effect model by ading new predictor year
+
+# Regularization with Year effect
+
+lambdas <- seq(0, 10, 0.25)
+
+rmses <- sapply(lambdas, function(l){
+  
+  mu <- mean(edx_train$rating)
+  
+  b_i <- edx_train %>% 
+    group_by(movieId) %>%
+    summarize(b_i = sum(rating - mu)/(n()+l))
+  
+  b_u <- edx_train %>% 
+    left_join(b_i, by="movieId") %>%
+    group_by(userId) %>% 
+    summarize(b_u = sum(rating - b_i - mu)/(n()+l))
+  
+  b_y <- edx_train %>%
+    left_join(b_i, by='movieId') %>%
+    left_join(b_u, by='userId') %>%
+    group_by(year) %>%
+    summarize(b_y = sum(rating - b_i - mu - b_u)/(n()+l))
+  
+  predicted_ratings <- 
+    edx_test %>% 
+    left_join(b_i, by = "movieId") %>%
+    left_join(b_u, by = "userId") %>%
+    left_join(b_y, by = "year") %>%
+    mutate(pred = mu + b_i + b_u + b_y) %>%
+    pull(pred)
+  
+  return(RMSE(predicted_ratings, edx_test$rating))
+})
+
+
+# Plot rmses vs lambdas to select the optimal lambda                                                             
+qplot(lambdas, rmses)  
+
+
+# The optimal lambda                                                             
+lambda <- lambdas[which.min(rmses)]
+lambda
+
+# Test and save results                                                             
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(Method="Regularized movie, user and year effect model",  
+                                     RMSE = min(rmses)))
+
+# Check result
+rmse_results %>% knitr::kable()
+
+# Final Model ( Movie + User Effect+year + Regularization) on full edx and Validation test
+
+lambdas <- seq(0, 10, 0.25)
+
+rmses <- sapply(lambdas, function(l){
+  
+  mu <- mean(edx$rating)
+  
+  b_i <- edx %>% 
+    group_by(movieId) %>%
+    summarize(b_i = sum(rating - mu)/(n()+l))
+  
+  b_u <- edx %>% 
+    left_join(b_i, by="movieId") %>%
+    group_by(userId) %>% 
+    summarize(b_u = sum(rating - b_i - mu)/(n()+l))
+  
+  b_y <- edx %>%
+    left_join(b_i, by='movieId') %>%
+    left_join(b_u, by='userId') %>%
+    group_by(year) %>%
+    summarize(b_y = sum(rating - b_i - mu - b_u)/(n()+l))
+  
+  predicted_ratings <- 
+    validation %>% 
+    left_join(b_i, by = "movieId") %>%
+    left_join(b_u, by = "userId") %>%
+    left_join(b_y, by = "year") %>%
+    mutate(pred = mu + b_i + b_u + b_y) %>%
+    pull(pred)
+  
+  return(RMSE(predicted_ratings, validation$rating))
+})
+
+
+# Plot rmses vs lambdas to select the optimal lambda                                                             
+qplot(lambdas, rmses)  
+
+
+# The optimal lambda                                                             
+lambda <- lambdas[which.min(rmses)]
+lambda
+
+# Test and save results                                                             
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(Method="Final model on full edx set and validation set",  
+                                     RMSE = min(rmses)))
+
+# Check result
+rmse_results %>% knitr::kable()
+
+#### Results ####                                                            
+# RMSE results overview                                                          
+rmse_results %>% knitr::kable()
+
+
+
+
+
+
+
+
